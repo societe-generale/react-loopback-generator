eb6fbba346ef1323fd8f61c0731153f753a021df
Jérémie Drouet <jeremie.drouet@gmail.com>
2016-07-25 15:21:27 +0200

    Add server port question

--- prompts/server.js
+++ prompts/server.js
@@ -1,5 +1,23 @@
 module.exports = {
 
+  port: function() {
+    if (!this.options['server-required']) {
+      return;
+    }
+    if (this.options['server-port']) {
+      this.log('server port already specified');
+      return;
+    }
+    return this.prompt({
+      type: 'input',
+      name: 'server-port',
+      message: 'Your server port',
+      default: 3000,
+    }).then(answers => {
+      this.options['server-port'] = answers['server-port'];
+    });
+  },
+
   required: function() {
     if (this.options['server-required'] !== undefined) {
       this.log('server requirement already specified');
