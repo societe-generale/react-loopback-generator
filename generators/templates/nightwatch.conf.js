e78beae0377d23081e71d9446cf7376257320e51
thibault-jacquet <thibault.jacquet90@gmail.com>
2016-11-09 14:17:10 +0100

    Add end2end test with Nightwatch

---
+++ generators/templates/nightwatch.conf.js
@@ -0,0 +1,30 @@
+module.exports = {
+  "src_folders": [
+    "test/e2e"// Where you are storing your Nightwatch e2e/UAT tests
+  ],
+  "output_folder": "./e2e_reports", // reports (test outcome) output by nightwatch
+  "selenium": { // downloaded by selenium-download module (see readme)
+    "start_process": true, // tells nightwatch to start/stop the selenium process
+    "server_path": "./node_modules/selenium-server-standalone-jar/jar/selenium-server-standalone-3.0.1.jar",
+    "cli_args": { // chromedriver is downloaded by selenium-download (see readme)
+      "webdriver.chrome.driver" : "./node_modules/chromedriver/bin/chromedriver"
+    }
+  },
+  "test_settings": {
+    "default": {
+      "globals": {
+        "waitForConditionTimeout": 5000 // sometimes internet is slow so wait.
+      },
+      "desiredCapabilities": {
+        "browserName": "chrome",
+        "javascriptEnabled": true // set to false to test progressive enhancement
+      }
+    }
+  },
+  "chrome": {
+    "desiredCapabilities": {
+      "browserName": "chrome",
+      "javascriptEnabled": true// set to false to test progressive enhancement
+    }
+  }
+};
---
+++ generators/templates/test/e2e/sample.test.js
@@ -0,0 +1,30 @@
+var URL = "https://localhost:8000/<%= applicationName %>/";
+
+module.exports = {
+  'Should display Gaia authentification page': function (client) {
+    client
+      .url(URL)
+      .useCss()
+      .waitForElementVisible('body', 1000)
+      .waitForElementPresent('input#sg-email', 1000)
+      .waitForElementPresent('input#sg-password', 1000)
+      .setValue('input#sg-email', 'user@socgen.com')
+      .setValue('input#sg-password', 'user')
+      .waitForElementPresent("button[type=submit]", 1000)
+  },
+  'Should display Main page of the Application': function (client) {
+    client
+      .click('button[type=submit]')
+      .waitForElementPresent('input#sg-pin', 1000)
+      .element('css selector', 'input#sg-pin-2', function (result) {
+        if (result.status !== -1) {
+          console.log("--------------IT'S YOUR FIRST TIME-----------\n You must set");
+        } else {
+          client.setValue('input#sg-pin', '1212121')
+            .click('button[type=submit]')
+            .assert.urlContains('<%= applicationName %>');
+        }
+      })
+
+  }
+};
