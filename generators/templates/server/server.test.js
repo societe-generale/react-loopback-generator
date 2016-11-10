6aa84568e13de3b627893c41e931afffe6aa60a5
thibault-jacquet <thibault.jacquet90@gmail.com>
2016-11-10 17:14:55 +0100

    Add test to the server and a test documentation

---
+++ generators/templates/server/server.test.js
@@ -0,0 +1,10 @@
+import expect from "expect";
+
+describe('server test', () => {
+
+  describe('fake test for demonstration', () => {
+    it('should return true', () => {
+      expect(true).toEqual(true);
+    })
+  })
+});
