3b6c828619f5475f8600bbb2ab03d79b4a43df34
thibault-jacquet <thibault.jacquet90@gmail.com>
2016-12-09 16:03:57 +0100

    Destructuring logout

--- generators/templates/client/source/components/hello-card/index.jsx
+++ generators/templates/client/source/components/hello-card/index.jsx
@@ -1,20 +1,30 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { Card, CardText } from 'material-ui/Card';
 
-const HelloCard = props => (
-  <div className="box">
-    <Card>
-      <CardText>
-        <h1>Hello {props.user.firstName}</h1>
-        <h2>Your roles: {props.user.roles.map(role => role.name).join(', ')}</h2>
-      </CardText>
-    </Card>
-  </div>);
+class HelloCard extends Component {
+  render() {
+    const { authentication: { user = { roles: [] } } } = this.props;
+    return (
+      <div className="box">
+        <Card>
+          <CardText>
+            <h1>Hello {user.firstName}</h1>
+            <h2>Your roles: {user.roles.map(role => role.name).join(', ')}</h2>
+          </CardText>
+        </Card>
+      </div>);
+  }
+}
 
 HelloCard.propTypes = {
-  user: PropTypes.shape({
-    firstName: PropTypes.string,
-    roles: PropTypes.array,
+  authentication: PropTypes.shape({
+    user: PropTypes.shape({
+      firstName: PropTypes.string,
+      roles: PropTypes.arrayOf(PropTypes.shape({
+        name: PropTypes.string,
+      }),
+      ),
+    }),
   }),
 };
 
