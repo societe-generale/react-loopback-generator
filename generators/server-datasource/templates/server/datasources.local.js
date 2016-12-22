f9504774f43e939b6082926aac5a8aa04fc87e64
thibault-jacquet <thibault.jacquet90@gmail.com>
2016-12-22 15:49:19 +0100

    Fix code style

--- generators/server-datasource/templates/server/datasources.local.js
+++ generators/server-datasource/templates/server/datasources.local.js
@@ -9,5 +9,3 @@ module.exports = {
     port: process.env.DATABASE_PORT,
   }
 };
-
-
--- generators/server/templates/server/datasources.local.js
+++ generators/server/templates/server/datasources.local.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   db: {
-      name: 'db',
-      connector: 'memory'
+    name: 'db',
+    connector: 'memory'
   }
 };
