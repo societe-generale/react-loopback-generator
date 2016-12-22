0e2f619bbf09ae404affad38e43392fc8c127475
thibault-jacquet <thibault.jacquet90@gmail.com>
2016-12-22 11:19:51 +0100

    Lost th fat destructuring

--- generators/client/templates/client/source/containers/home-view/index.jsx
+++ generators/client/templates/client/source/containers/home-view/index.jsx
@@ -1,33 +1,28 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import HelloCard from '../../components/hello-card';
 
 class HomeView extends Component {
-
   render() {
+    const { authentication = {} } = this.props;
     return (
       <div className="container">
         <div className="row center-xs">
           <div className="col-xs-12 col-md-6">
-            <HelloCard user={this.props.user} />
+            <HelloCard authentication={authentication} />
           </div>
         </div>
       </div>
     );
   }
-
 }
 
-HomeView.propTypes = {
-  user: PropTypes.shape({
-    firstName: PropTypes.string,
-  }),
-};
+HomeView.propTypes = HelloCard.propTypes;
 
 function mapStateToProps(state) {
   return {
-    user: state.authentication.user,
+    authentication: state.authentication,
   };
 }
 
