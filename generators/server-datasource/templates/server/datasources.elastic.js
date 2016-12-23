53579124b33b386011bbb26f4b7e869ee844edd3
Antoine Toubhans <antoine@toubhans.org>
2016-12-23 15:46:39 +0100

    Add schema in datasource and database

--- generators/server-datasource/templates/server/datasources.elastic.js
+++ generators/server-datasource/templates/server/datasources.elastic.js
@@ -5,6 +5,7 @@ module.exports = {
     database: process.env.DATABASE_DB,
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
+    schema: process.env.DATABASE_SCHEMA,
     host: process.env.DATABASE_HOST,
     port: process.env.DATABASE_PORT,
   },
