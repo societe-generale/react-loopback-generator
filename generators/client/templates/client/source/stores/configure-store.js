92d1cf8254a4dbd93b7d6b57d6b69984dcc2771b
Daniel Jorge <dani2013@gmail.com>
2017-03-23 21:34:56 +0100

    Added redux devtools ext support

--- generators/client/templates/client/source/stores/configure-store.js
+++ generators/client/templates/client/source/stores/configure-store.js
@@ -1,7 +1,6 @@
 import { applyMiddleware, createStore, compose } from 'redux';
 import { routerMiddleware } from 'react-router-redux';
 import thunk from 'redux-thunk';
-import createLogger from 'redux-logger';
 import persistState, { mergePersistedState } from 'redux-localstorage';
 import adapter from 'redux-localstorage/lib/adapters/localStorage';
 import filter from 'redux-localstorage-filter';
@@ -9,16 +8,20 @@ import filter from 'redux-localstorage-filter';
 import rootReducer from '../reducers';
 
 export default function configureStore(history) {
-  const logger = createLogger({
-    level: process.env.NODE_ENV === 'production' ? 'error' : 'log',
-    collapsed: true,
-  });
   const reduxRouterMiddleware = routerMiddleware(history);
   const reducers = compose(mergePersistedState())(rootReducer);
   const storageAuth = compose(filter('authentication'))(adapter(window.localStorage));
   const storageLang = compose(filter('language'))(adapter(window.localStorage));
 
-  let createCustomStore = compose(
+  /* eslint-disable no-underscore-dangle */
+  const composeEnhancers =
+    process.env.NODE_ENV !== 'production' &&
+      typeof window === 'object' &&
+      window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+      window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : compose;
+  /* eslint-enable */
+
+  let createCustomStore = composeEnhancers(
     persistState(storageAuth, 'authentication'),
     persistState(storageLang, 'language'),
   )(createStore);
@@ -27,10 +30,6 @@ export default function configureStore(history) {
     reduxRouterMiddleware,
     thunk,
   ];
-
-  if (process.env.NODE_ENV !== 'production') {
-    middlewares.push(logger);
-  }
 
   createCustomStore = applyMiddleware(...middlewares)(createCustomStore);
 
