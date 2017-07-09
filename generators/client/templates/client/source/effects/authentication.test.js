15d473d343052c6ee0fbb4800a157e2e9f7f2782
Hugo CAPOCCI <hcapocci@degetel.com>
2017-07-09 23:57:11 +0200

    Fix client tests

--- generators/client/templates/client/source/effects/authentication.test.js
+++ generators/client/templates/client/source/effects/authentication.test.js
@@ -4,6 +4,7 @@ import thunk from 'redux-thunk';
 import nock from 'nock';
 import * as effect from './authentication';
 import url from '../constants/url-config';
+import { data } from './fakeuser';
 
 const middleware = [thunk];
 const mockStore = configureMockStore(middleware);
@@ -12,7 +13,12 @@ describe('Actions authentication', () => {
   it('should call request and  dispatch AUTHENTICATION_LOGIN', () => {
     url.AUTH = 'http://localhost/api/auth';
     const store = mockStore({});
-    const expected = [{ type: 'AUTHENTICATION_LOGIN', payload: { user: 'myUser' } }];
+    const expected = [{
+      type: 'AUTHENTICATION_LOGIN',
+      payload: {
+        user: data.user,
+      },
+    }];
     nock('http://localhost')
       .get('/api/auth')
       .reply(200, { user: 'myUser' });
